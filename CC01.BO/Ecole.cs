06e71b2919993ab30ae78f9b1682e3598620b550
Daryl-gif <68334153+Daryl-gif@users.noreply.github.com>
2020-11-25 14:42:05 +0100

    Update Ecole.cs
    Ajout final Classe Ecole

--- CC01.BO/Ecole.cs
+++ CC01.BO/Ecole.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace CC01.BO
 {
-    class Ecole
+    [Serializable]
+    public class Ecole
     {
+        public string Nom { get; set; }
+        public string Lieu { get; set; }
+        public long Telephone { get; set; }
+         
+        public Ecole() {
+            
+        }
+        public Ecole(string nom,string lieu, long telephone)
+        {
+            Nom = nom;
+            Lieu = lieu;
+            Telephone = telephone;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Ecole ecole &&
+                   Nom == ecole.Nom &&
+                   Lieu == ecole.Lieu &&
+                   Telephone == ecole.Telephone;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 381545344;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nom);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Lieu);
+            hashCode = hashCode * -1521134295 + Telephone.GetHashCode();
+            return hashCode;
+        }
     }
 }
